43f7fa32b650c408ef439a8a41909e0f2eabfee2
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2022-10-29 20:56:12 +0100

    fixed issue with cursor and go fmt

--- nvim/.config/nvim/lua/panagiotisptr/init.lua
+++ nvim/.config/nvim/lua/panagiotisptr/init.lua
@@ -154,9 +154,9 @@ local lspconfig = require('lspconfig')
 local lsp_defaults = lspconfig.util.default_config
 
 lsp_defaults.capabilities = vim.tbl_deep_extend(
-  'force',
-  lsp_defaults.capabilities,
-  require('cmp_nvim_lsp').default_capabilities()
+    'force',
+    lsp_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
 )
 
 -- Mappings.
@@ -170,32 +170,43 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<space>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap=true, silent=true, buffer=bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+
+    if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format()
+            end,
+        })
+    end
 end
 
 local lsp_flags = {
-  -- This is the default in Nvim 0.7+
-  debounce_text_changes = 150,
+    -- This is the default in Nvim 0.7+
+    debounce_text_changes = 150,
 }
 
 lspconfig.sumneko_lua.setup({
@@ -206,28 +217,26 @@ lspconfig.gopls.setup({
 })
 
 local org_imports = function()
-  local clients = vim.lsp.buf_get_clients()
-  for _, client in pairs(clients) do
+    local clients = vim.lsp.buf_get_clients()
+    for _, client in pairs(clients) do
 
-    local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
-    params.context = {only = {"source.organizeImports"}}
+        local params = vim.lsp.util.make_range_params(nil, client.offset_encoding)
+        params.context = {only = {"source.organizeImports"}}
 
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 5000)
-    for _, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
-        else
-          vim.lsp.buf.execute_command(r.command)
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 5000)
+        for _, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+                if r.edit then
+                    vim.lsp.util.apply_workspace_edit(r.edit, client.offset_encoding)
+                else
+                    vim.lsp.buf.execute_command(r.command)
+                end
+            end
         end
-      end
     end
-  end
 end
 
-vim.cmd [[autocmd BufWritePre *.go %!gofmt]]
-
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.go" },
-  callback = org_imports,
+    pattern = { "*.go" },
+    callback = org_imports,
 })
--- nvim/.config/nvim/lua/panagiotisptr/remap.lua
+++ nvim/.config/nvim/lua/panagiotisptr/remap.lua
@@ -1,5 +1,6 @@
 local nnoremap = require('panagiotisptr.keymap').nnoremap
 local inoremap = require('panagiotisptr.keymap').inoremap
+local vnoremap = require('panagiotisptr.keymap').vnoremap
 
 local telescopeBuilin = require('telescope.builtin')
 
@@ -9,5 +10,6 @@ nnoremap('<leader>pv', '<cmd>Ex<CR>')
 nnoremap('<leader>ff', telescopeBuilin.find_files, {})
 nnoremap('<leader>fg', telescopeBuilin.live_grep, {})
 nnoremap('<leader>fb', telescopeBuilin.buffers, {})
+vnoremap('<leader>y', '"*yy')
 
 

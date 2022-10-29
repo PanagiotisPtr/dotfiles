1e6677e675ea4c448b006892f1390aedcaf3d226
PanagiotisPtr <lpanagiotispetridisl@gmail.com>
2022-10-29 20:28:02 +0100

    fixed lsp issues

--- nvim/.config/nvim/lua/panagiotisptr/init.lua
+++ nvim/.config/nvim/lua/panagiotisptr/init.lua
@@ -20,7 +20,6 @@ require'nvim-treesitter.configs'.setup {
 
     highlight = {
         enable = true,
-
         additional_vim_regex_highlighting = false,
     },
 
@@ -30,7 +29,7 @@ require'nvim-treesitter.configs'.setup {
 
     refactor = {
         highlight_definitions = {
-            enable = true,
+            enable = false,
             clear_on_cursor_move = true,
         },
         smart_rename = {
@@ -160,10 +159,53 @@ lsp_defaults.capabilities = vim.tbl_deep_extend(
   require('cmp_nvim_lsp').default_capabilities()
 )
 
-lspconfig.sumneko_lua.setup({})
-lspconfig.gopls.setup({})
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
-local function org_imports()
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+lspconfig.sumneko_lua.setup({
+    on_attach = on_attach,
+})
+lspconfig.gopls.setup({
+    on_attach = on_attach,
+})
+
+local org_imports = function()
   local clients = vim.lsp.buf_get_clients()
   for _, client in pairs(clients) do
 
